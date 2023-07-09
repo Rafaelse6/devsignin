66679d0c6c7a29146ba5ed83cd7e5ca1545a7781
Rafael Santos <rafa.05125@gmail.com>
2023-07-09 15:59:21 -0300

    Users schema, auth imported in user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersSchema } from './schemas/users.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UsersSchema,
+      },
+    ]),
+    AuthModule,
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
